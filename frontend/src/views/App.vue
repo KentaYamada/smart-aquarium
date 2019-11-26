b3a3df17f72573304bebd1d490dc87bc171f2382
Kenta Yamada <knt.yamada.800@gamil.com>
2019-11-26 11:52:55 +0900

    move App.vue

--- frontend/src/views/App.vue
+++ frontend/src/views/App.vue
@@ -2,9 +2,13 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/users">User</router-link>
+      <router-link to="/login">Login</router-link>
     </div>
-    <router-view/>
+    <div class="container">
+        <router-view/>
+    </div>
   </div>
 </template>
 
