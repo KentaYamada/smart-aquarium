fe3498f63785dd3452b1a2d8c11ba44ae9968be7
Kenta Yamada <knt.yamada.800@gamil.com>
2019-11-26 11:53:21 +0900

    move App.vue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Buefy from 'buefy';
 import _ from 'lodash';
-import App from './App.vue';
+import App from '@/views/App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
