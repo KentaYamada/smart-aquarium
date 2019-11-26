ea36a76d176a88aff00c0f95cd2d6319e2cbb8d3
Kenta Yamada <knt.yamada.800@gamil.com>
2019-11-26 12:43:34 +0900

    Implement sample user setting ui

---
+++ frontend/src/views/user/edit/script.ts
@@ -0,0 +1,47 @@
+import Vue from 'vue';
+import _ from 'lodash';
+import { mapActions } from 'vuex';
+import { AxiosError, AxiosResponse } from 'axios';
+import { ToastConfig } from 'buefy/types/components';
+import { User } from '@/entity/user';
+import { SAVE } from '@/store/constant';
+import { SAVE_SUCCESS } from '@/views/constant';
+
+
+export default Vue.extend({
+    props: {
+        user: {
+            required: true,
+            type: User,
+        },
+    },
+    data() {
+        return {
+            errors: {},
+        };
+    },
+    methods: {
+        ...mapActions('user', [
+            SAVE,
+        ]),
+        handleSave(): void {
+            this.SAVE(this.user)
+                .then((response: AxiosResponse) => {
+                    this.$emit('close');
+                    this.$emit(SAVE_SUCCESS, response.data.message);
+                })
+                .catch((error: AxiosError) => {
+                    if (!_.isNil(error.response.error)) {
+                        this.errors = error.response.error;
+                    }
+
+                    const config: ToastConfig = {
+                        message: error.response.data.message,
+                        type: 'is-danger',
+                    };
+                    this.$toast.open(config);
+                });
+        },
+    },
+});
+
