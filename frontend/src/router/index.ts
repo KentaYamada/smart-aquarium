c5e3e18a6aff418d0bc6e25b15bfdd6aec945c0f
Kenta Yamada <knt.yamada.800@gamil.com>
2019-11-26 12:43:46 +0900

    Implement router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,23 +1,39 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import LoginForm from '@/views/login/LoginForm.vue';
+import UserList from '@/views/user/list/UserList.vue';
+
 
 Vue.use(VueRouter);
 
+//const routes = [
+//  {
+//    path: '/',
+//    name: 'home',
+//    component: Home,
+//  },
+//  {
+//    path: '/about',
+//    name: 'about',
+//    // route level code-splitting
+//    // this generates a separate chunk (about.[hash].js) for this route
+//    // which is lazy-loaded when the route is visited.
+//    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+//  },
+//];
+
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+    {
+        path: '/users',
+        name: 'user',
+        component: UserList,
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: LoginForm,
+    },
 ];
 
 const router = new VueRouter({
