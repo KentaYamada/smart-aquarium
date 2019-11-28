888a62b03058f0f47b65f1d43fd045168a35b410
Kenta Yamada <knt.yamada.800@gamil.com>
2019-11-28 13:54:01 +0900

    refactoring

--- frontend/src/views/App.vue
+++ frontend/src/views/App.vue
@@ -1,40 +1,11 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/users">User</router-link>
-      <router-link to="/login">Login</router-link>
-    </div>
-    <navigation/>
-    <div class="container">
-        <router-view/>
-    </div>
-  </div>
+    <section class="section">
+        <navigation/>
+        <div class="container">
+            <router-view/>
+        </div>
+    </section>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
 
 <script lang="ts">
 import Vue from 'vue';
@@ -46,4 +17,5 @@ export default Vue.extend({
         Navigation
     }
 });
+
 </script>
