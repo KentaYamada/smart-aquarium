96dfea8447b879fc3390add2527d1cef34f6ea28
Kenta Yamada <knt.yamada.800@gamil.com>
2019-12-05 22:15:15 +0900

    #11 ナビゲーションをVuexで実装

--- frontend/src/components/navigation/script.ts
+++ frontend/src/components/navigation/script.ts
@@ -1,33 +1,22 @@
 import Vue from 'vue';
 import { mapGetters, mapActions } from 'vuex';
 import { ToastConfig } from 'buefy/types/components';
-import { IS_LOGGED_IN, LOGOUT } from '@/store/constant';
+import { IS_LOGGED_IN, LOGOUT, NAVIGATIONS } from '@/store/constant';
 
-const navigations = [
-    {
-        title: '設定',
-        icon: 'fa-user-cog',
-        submenu: [
-            {
-                title: 'ユーザー',
-                icon: 'fa-user',
-                url: '/users',
-            },
-        ],
-    },
-];
 
 export default Vue.extend({
     template: '<navigation/>',
     data() {
         return {
-            navigations,
             isShowNavi: false,
         };
     },
     computed: {
         ...mapGetters('auth', [
             IS_LOGGED_IN,
+        ]),
+        ...mapGetters('navigation', [
+            NAVIGATIONS,
         ]),
     },
     methods: {
