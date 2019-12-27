1a8a8468a406eddc68471ff4c3a3d196420005b4
Kenta Yamada <knt.yamada.800@gamil.com>
2019-12-28 00:01:21 +0900

    #1 ナビゲーションメニューに認証部分をなくす

--- frontend/src/components/navigation/script.ts
+++ frontend/src/components/navigation/script.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import { mapGetters, mapActions } from 'vuex';
 import { ToastConfig } from 'buefy/types/components';
-import { IS_LOGGED_IN, LOGOUT, NAVIGATIONS } from '@/store/constant';
+import { NAVIGATIONS } from '@/store/constant';
 
 
 export default Vue.extend({
@@ -12,46 +12,16 @@ export default Vue.extend({
         };
     },
     computed: {
-        ...mapGetters('auth', [
-            IS_LOGGED_IN,
-        ]),
         ...mapGetters('navigation', [
             NAVIGATIONS,
         ]),
     },
     methods: {
-        ...mapActions('auth', [
-            LOGOUT,
-        ]),
         handleToggleNavi(): void {
             this.isShowNavi = !this.isShowNavi;
         },
         handleCloseNavi(): void {
             this.isShowNavi = false;
-        },
-        handleLogout(): void {
-            this.logout()
-                .then(() => {
-                    this._logoutSucceeded();
-                })
-                .catch(() => {
-                    this._logoutFailure();
-                });
-        },
-        _logoutSucceeded(): void {
-            const config: ToastConfig = {
-                message: 'ログアウトしました',
-                type: 'is-success',
-            };
-            this.$buefy.toast.open(config);
-            this.$router.push('/login');
-        },
-        _logoutFailure(): void {
-            const config: ToastConfig = {
-                message: 'ログアウトに失敗しました',
-                type: 'is-danger',
-            };
-            this.$buefy.toast.open(config);
         },
     },
 });
