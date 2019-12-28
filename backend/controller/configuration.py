157afab0f2579d86a4e2eb0569a21cec9420fec7
Kenta Yamada <knt.yamada.800@gamil.com>
2019-12-28 12:36:37 +0900

    #3 [backend] 設定情報API実装

--- backend/controller/configuration.py
+++ backend/controller/configuration.py
@@ -2,15 +2,15 @@ from flask import request, Blueprint
 from werkzeug.exceptions import (
     BadRequest,
     Conflict,
-    NotFound
+    NotFound,
 )
 from backend.libs.api_response import (
     STATUS_OK,
-    STATUS_CREATED,
-    STATUS_NO_CONTENT,
     ApiResponseBody,
     ApiResponse
 )
+from backend.mapper.configuration import ConfigurationMapper
+from backend.model.configuration import Configuration
 
 
 bp = Blueprint('configuration', __name__, url_prefix='/api/configurations')
@@ -18,9 +18,37 @@ bp = Blueprint('configuration', __name__, url_prefix='/api/configurations')
 
 @bp.route('/', methods=['GET'])
 def index():
-    pass
+    data = ConfigurationMapper.find()
+    if data is None:
+        raise NotFound(description='設定情報が見つかりません。')
+    body = ApiResponseBody()
+    body.configuration = data
+    return ApiResponse(STATUS_OK, body)
 
 
-@bp.route('/', methods=['PUT'])
-def edit():
-    pass
+@bp.route('/', methods=['POST'])
+def apply():
+    data = request.get_json()
+    if data is None:
+        raise BadRequest()
+
+    white_list = {
+        'ph_lower_limit',
+        'ph_upper_limit',
+        'temperature_lower_limit',
+        'temperature_upper_limit',
+        'measurement_trials'
+    }
+    if not data.keys() >= white_list:
+        raise BadRequest(description='不正なリクエストです')
+
+    model = Configuration(**data)
+    saved = ConfigurationMapper.save(model)
+
+    if not saved:
+        raise Conflict(description='設定の反映に失敗しました')
+
+    body = ApiResponseBody()
+    body.message = '設定反映しました'
+    body.result = True
+    return ApiResponse(STATUS_OK, body)
--- backend/tests/controller/test_configuration.py
+++ backend/tests/controller/test_configuration.py
@@ -1,16 +1,78 @@
 import json
-from urllib.parse import urljoin
+from unittest.mock import patch
 from backend.libs.api_response import (
     STATUS_OK,
-    STATUS_CREATED,
     STATUS_CONFLICT,
     STATUS_BAD_REQUEST,
-    STATUS_METHOD_NOT_ALLOWED,
-    STATUS_NOT_FOUND,
-    STATUS_NO_CONTENT
 )
 from backend.tests.controller.base import BaseApiTestCase
 
 
 class TestConfigurationApi(BaseApiTestCase):
-    pass
+    END_POINT = '/api/configurations/'
+
+    def test_index(self):
+        res = self.client.get(
+            self.END_POINT,
+            content_type=self.CONTENT_TYPE
+        )
+        self.assertEqual(STATUS_OK, res.status_code)
+
+    def test_apply(self):
+        data = json.dumps({
+            'ph_lower_limit': 6.7,
+            'ph_upper_limit': 6.9,
+            'temperature_lower_limit': 23.0,
+            'temperature_upper_limit': 23.5,
+            'measurement_trials': 3
+        })
+        res = self.client.post(
+            self.END_POINT,
+            content_type=self.CONTENT_TYPE,
+            data=data
+        )
+        body = json.loads(res.get_data())
+        self.assertEqual(STATUS_OK, res.status_code)
+        self.assertTrue(body['result'])
+        self.assertEqual(body['message'], '設定反映しました')
+
+    def test_apply_failure_when_request_no_data(self):
+        res = self.client.post(
+            self.END_POINT,
+            content_type=self.CONTENT_TYPE,
+        )
+        self.assertEqual(STATUS_BAD_REQUEST, res.status_code)
+
+    def test_apply_failure_when_invalid_request_data(self):
+        data = json.dumps({
+            'ph_lower_limit': 6.7,
+            'temperature_lower_limit': 23.0,
+            'measurement_trials': 3
+        })
+        res = self.client.post(
+            self.END_POINT,
+            content_type=self.CONTENT_TYPE,
+            data=data
+        )
+        body = json.loads(res.get_data())
+        self.assertEqual(STATUS_BAD_REQUEST, res.status_code)
+        self.assertEqual(body['message'], '不正なリクエストです')
+
+    @patch('backend.mapper.configuration.ConfigurationMapper.save')
+    def test_apply_failure_when_save_failed(self, mock_save):
+        mock_save.return_value = False
+        data = json.dumps({
+            'ph_lower_limit': 6.7,
+            'ph_upper_limit': 6.9,
+            'temperature_lower_limit': 23.0,
+            'temperature_upper_limit': 23.5,
+            'measurement_trials': 3
+        })
+        res = self.client.post(
+            self.END_POINT,
+            content_type=self.CONTENT_TYPE,
+            data=data
+        )
+        body = json.loads(res.get_data())
+        self.assertEqual(STATUS_CONFLICT, res.status_code)
+        self.assertEqual(body['message'], '設定の反映に失敗しました')
