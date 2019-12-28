d18ed266e3b9d3cf630a8256e0f66adfe376be1a
Kenta Yamada <knt.yamada.800@gamil.com>
2019-12-28 20:40:30 +0900

    #4 [frontend] ナビゲーションで通知一覧を表示できるようにする

--- frontend/src/components/navigation/script.ts
+++ frontend/src/components/navigation/script.ts
@@ -1,7 +1,15 @@
 import Vue from 'vue';
-import { mapGetters, mapActions } from 'vuex';
+import {
+    mapActions,
+    mapGetters,
+    mapState,
+} from 'vuex';
 import { ToastConfig } from 'buefy/types/components';
-import { NAVIGATIONS } from '@/store/constant';
+import {
+    FETCH,
+    HAS_ITEMS,
+    NAVIGATIONS
+} from '@/store/constant';
 
 
 export default Vue.extend({
@@ -11,12 +19,24 @@ export default Vue.extend({
             isShowNavi: false,
         };
     },
+    mounted() {
+        this.fetch();
+    },
     computed: {
         ...mapGetters('navigation', [
             NAVIGATIONS,
         ]),
+        ...mapGetters('notification', [
+            HAS_ITEMS,
+        ]),
+        ...mapState('notification', [
+            'notifications',
+        ]),
     },
     methods: {
+        ...mapActions('notification', [
+            FETCH,
+        ]),
         handleToggleNavi(): void {
             this.isShowNavi = !this.isShowNavi;
         },
